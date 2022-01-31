840d384b1c15edd977cdcb7877933ed77b167162
WaitForMeh <94563710+WaitForMeh@users.noreply.github.com>
2022-01-31 22:47:55 +0100

    Initial commit

---
+++ Events/ready.js
@@ -0,0 +1,80 @@
+const { REST } = require("@discordjs/rest"),
+    { Routes } = require("discord-api-types/v9"),
+    Discord = require("discord.js"),
+    Utils = require("../Modules/Utils"),
+    packageJSON = require("../package.json"),
+    chalk = require("chalk");
+/**
+ *
+ * @param {Discord.Client} bot
+ * @param {Discord.Interaction} interaction
+ */
+module.exports = async (bot) => {
+    let { SlashCmds, SlashCmdsData, config } = bot;
+
+    await Utils.logInfo(
+        "#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#"
+    );
+    await Utils.logInfo(
+        "                                                                          "
+    );
+    await Utils.logInfo(
+        `                    • ${chalk.bold(
+            `Brayan Bot v${packageJSON.version}`
+        )} is now Online! •       `
+    );
+    await Utils.logInfo(
+        "                                                                          "
+    );
+    await Utils.logInfo(
+        "          • Join our Discord Server for any Issues/Custom Bots •          "
+    );
+    await Utils.logInfo(
+        `                     ${chalk.blue(
+            chalk.underline(`https://discord.gg/EgeZxGg6ev`)
+        )}                        `
+    );
+    await Utils.logInfo(
+        "                                                                          "
+    );
+    await Utils.logInfo(
+        "#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#"
+    );
+
+    await Utils.logInfo(`${chalk.bold(bot.Commands.size)} Command${bot.Commands.size == 1 ? "" : "s"} Loaded.`);
+    await Utils.logInfo(`${chalk.bold(bot.Events.length)} Event${bot.Events.length == 1 ? "" : "s"} Loaded.`);
+    const rest = new REST({ version: "9" }).setToken(config.Settings.Token);
+    SlashCmdsData = SlashCmdsData.filter((x) => typeof x == "object");
+    try {
+        await rest.put(
+            Routes.applicationGuildCommands(
+                bot.user.id,
+                config.Settings.ServerID
+            ),
+            {
+                body: SlashCmdsData,
+            }
+        );
+        await Utils.logInfo(
+            `${chalk.bold(SlashCmdsData.length)} Slash Command${SlashCmdsData.length == 1 ? "" : "s"} Loaded.`
+        );
+    } catch (error) {
+        console.log(error);
+        Utils.logError(error);
+    }
+
+    await Utils.logInfo(`Logged in as: ${chalk.bold(bot.user.tag)}`);
+    bot.guilds.cache.size > 1
+        ? Utils.logWarning(
+            `Currently in ${chalk.bold(
+                bot.guilds.cache.size
+            )} servers. | ${chalk.hex("##ff596d")(
+                `Brayan Bot is not made for multiple servers.`
+            )}`
+        )
+        : Utils.logInfo(
+            `Currently in ${chalk.bold(bot.guilds.cache.size)} server.`
+        );
+    await Utils.logInfo(`Bot Ready!`);
+};
+module.exports.once = true;
---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center">
+  BrayanBot
+  <br>
+  <a href="https://zorino.in/discord">
+    <img border="0" alt="Discord" src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white">
+  </a>
+  <a href="https://www.javascript.com/">
+    <img border="0" alt="JavaScript" src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E">
+  </a>
+  <a href="https://nodejs.org/">
+    <img border="0" alt="Node.js" src="https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white">
+  </a>
+</h1>
+
+## 📢 About BrayanBot
+
+BrayanBot servers as base discord.js bot, on which developers can build their own add-ons. It is fully customizable - meaning with add-ons it can be turned into a moderation bot, music bot, ticket bot, and so on! You can enable/disable the features to your liking. It is a self-hosted bot - you need to host and maintain your own instance.
+
+## 🧩 Addons
+
+BrayanBot is fully modular, it allows you to install verified Third-Party Addons to extend its functionality. As per your requirements, you can use addons to turn it into the type of bot you need! Addons can be added to turn it into a full-fledged discord bot created by our ever-growing community of developers.
+
+Current Addon Developers:
+
+-   Lloyd#4414: [Discord](https://discord.gg/PCNMS37CYk)
+
+## 🚧 Prerequisites
+
+-   [Node.js 16.6+](https://nodejs.org/en/download/)
+-   [Discord.js](https://www.npmjs.com/package/discord.js/v/13.3.1)
+-   [Discord Bot Hosting](https://revivenode.com/discord.html) / [Linux Machine](https://github.com/DakshNamdev/BrayanBot/wiki/Hosting) / [Windows Server](https://github.com/DakshNamdev/BrayanBot/wiki/Hosting)
+
+## 👋 Join the community!
+
+BrayanBot is driven by the awesome community of third-party developers, which makes it more functional and enjoyable for everyone. If you are looking into building your own Add-ons for Brayan, consider looking into the documentation to get started!
+
+Need help? Join our [Discord Server](https://discord.gg/EgeZxGg6ev)!
+
+## ✨ Contributing
+
+Contributions of any kind are welcome! Please feel free to submit them to the repository via pull requests.
+
+<a href="https://github.com/DakshNamdev/BrayanBot/graphs/contributors">
+  <img src="https://contributors-img.web.app/image?repo=DakshNamdev/BrayanBot" width="150"/>
+</a>
